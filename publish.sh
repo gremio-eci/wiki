ff468664a437e9fc51b26e328d28ed45b2c0c74a
Paulo <paulosanz@poli.ufrj.br>
2020-01-29 02:23:31 -0300

    Publish to gh-pages

--- publish.sh
+++ publish.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-hugo -D -d docs
-git add docs/
+git stash
+git checkout gh-pages
+git reset --hard master
+
+hugo -D -v
+
+mkdir tmp
+mv * tmp/
+mv .* tmp/
+mv tmp/public/* .
+mv tmp/.git .
+mv tmp/.gitignore .
+rm -rf tmp
+
+git add -A
 git commit -m "Publish"
-git push origin master
+git push origin gh-pages
+
+git checkout master
+git stash apply
