96686466878c68fb5ed0e87a0b4927b687077aeb
Paulo <paulosanz@poli.ufrj.br>
2020-01-29 02:28:16 -0300

    Fix publish.sh

--- publish.sh
+++ publish.sh
@@ -10,12 +10,11 @@ mv * tmp/
 mv .* tmp/
 mv tmp/public/* .
 mv tmp/.git .
-mv tmp/.gitignore .
 rm -rf tmp
 
 git add -A
 git commit -m "Publish"
-git push origin gh-pages
+git push origin gh-pages -f
 
 git checkout master
 git stash apply
