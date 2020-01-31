b5adbed460bcb1181000f44c6a489bda75461100
Paulo <paulosanz@poli.ufrj.br>
2020-01-31 02:45:18 -0300

    Format script

--- Installer.ps1
+++ Installer.ps1
@@ -1,20 +1,17 @@
 ﻿Set-ExecutionPolicy Bypass -Scope Process -Force;
 $Packages = 'hugo', 'git'
 
-If(Test-Path -Path "$env:ProgramData\Chocolatey"){
+If (Test-Path -Path "$env:ProgramData\Chocolatey"){
+  ForEach ($PackageName in $Packages){
+    choco install $PackageName -y
+  }
+} Else{
+  $InstallDir='C:\ProgramData\chocoportable'
+  $env:ChocolateyInstall="$InstallDir"
+  Set-ExecutionPolicy Bypass -Scope Process -Force;
+  iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-	ForEach ($PackageName in $Packages){
-        	choco install $PackageName -y
-	}
-}
-
-Else{
-	$InstallDir='C:\ProgramData\chocoportable'
-        $env:ChocolateyInstall="$InstallDir"
-        Set-ExecutionPolicy Bypass -Scope Process -Force;
-        iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-	ForEach ($PackageName in $Packages){
-        	choco install $PackageName -y
-	}
+  ForEach ($PackageName in $Packages){
+    choco install $PackageName -y
+  }
 }
