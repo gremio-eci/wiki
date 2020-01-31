52093d24d4046bfc51d5054195a8da1268332830
Paulo <paulosanz@poli.ufrj.br>
2020-01-30 21:37:55 -0300

    Create markdown and contribution tutorial
    - Created tutorial/contribution.md file, with a thorough explanation of how this wiki works, how to run it locally and how to change it
    - Created tutorial/markdown.md with a basic reference to Markdown and more sources

--- publish.sh
+++ publish.sh
@@ -12,22 +12,33 @@ if ! [ -x "$(command -v hugo)" ]; then
   exit 1
 fi
 
-git stash
+if [ "$(git stash)" == "No local changes to save" ]; then
+  STASHED=false
+else
+  STASHED=true
+fi
+
 git checkout gh-pages
 git reset --hard master
 
 hugo -D -v
 
-mkdir tmp
-mv * tmp/
-mv .* tmp/
-mv tmp/public/* .
-mv tmp/.git .
-rm -rf tmp
-
-git add -A
-git commit -m "Publish"
-git push origin gh-pages -f
+if [ $? -ne 0 ]; then
+  echo "Hugo failed to run, aborting publishing"
+else
+  mkdir tmp 2> /dev/null
+  mv * tmp/ 2> /dev/null
+  mv .* tmp/ 2> /dev/null
+  mv tmp/public/* . 2> /dev/null
+  mv tmp/.git . 2> /dev/null
+  rm -rf tmp 2> /dev/null
+  git add -A
+  git commit -m "Publish"
+  git push origin gh-pages -f
+fi
 
 git checkout master
-git stash apply
+
+if [ "$STASHED" == true ]; then
+  git stash apply
+fi
